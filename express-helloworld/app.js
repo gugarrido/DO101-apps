5de456e5da893a3d5a6ba45a74708c3263d010b4
gugarrido <gustavo.garrido@ibm.com>
2020-05-19 00:32:58 -0300

    login page

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/login', function (req, req)) {
+  res.sendFile('/login.html', {root:__dirname})
+ }
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
